b970840892c79896b85eba7868e6421a455790c5
charmawhorms <charmawhorms27@gmail.com>
2026-04-10 02:00:12 -0500

    revert changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,18 +17,6 @@ kotlin {
         }
     }
 
-    // FOR IOS ---
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         moduleName = "composeApp"
@@ -41,17 +29,16 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
-                implementation(compose.components.resources)
-                implementation(compose.components.uiToolingPreview)
-            }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
         }
-        val androidMain by getting {
+
+        androidMain {
             dependencies {
                 implementation(compose.preview)
                 implementation("androidx.core:core-ktx:1.13.1")
@@ -62,20 +49,6 @@ kotlin {
                 implementation("androidx.room:room-runtime:2.6.1")
                 implementation("androidx.room:room-ktx:2.6.1")
             }
-        }
-
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-
-        val iosMain by creating {
-            dependsOn(commonMain)
-            dependencies {
-                implementation(compose.ui)
-            }
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
         }
     }
 }
--- app/src/iosMain/kotlin/com/ucc/itmobileapp/MainViewController.kt
+++
@@ -1,8 +0,0 @@
-package com.ucc.itmobileapp
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-// Lowercase 'm' to satisfy the naming warning
-fun mainViewController() = ComposeUIViewController {
-    UccWebApp()
-}
